11a74c6c0861ff6938baef3c4c7baec780a9dc75
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2025-01-13 17:00:11 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/spring_security_with_deep_dive/SpringSecurityWithDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.spring_security_with_deep_dive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithDeepDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithDeepDiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/zhelezyaka/spring_security_with_deep_dive/SpringSecurityWithDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.spring_security_with_deep_dive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithDeepDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
