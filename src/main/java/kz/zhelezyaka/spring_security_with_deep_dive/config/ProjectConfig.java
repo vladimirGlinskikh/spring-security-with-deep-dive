0e9ecb6c7c94fb6e47735a3bbbe5a6ace6281385
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2025-01-17 17:25:25 +0300

    Setting up UserDetailsService using the component SecurityFilterChain

--- src/main/java/kz/zhelezyaka/spring_security_with_deep_dive/config/ProjectConfig.java
+++ src/main/java/kz/zhelezyaka/spring_security_with_deep_dive/config/ProjectConfig.java
@@ -2,26 +2,36 @@ package kz.zhelezyaka.spring_security_with_deep_dive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    UserDetailsService userDetailsService() {
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
         var user = User.withUsername("Vladimir")
                 .password("123")
                 .authorities("read")
                 .build();
-        return new InMemoryUserDetailsManager(user);
+        var userDetailsService =
+                new InMemoryUserDetailsManager(user);
+
+        http.userDetailsService(userDetailsService);
+        return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 }
