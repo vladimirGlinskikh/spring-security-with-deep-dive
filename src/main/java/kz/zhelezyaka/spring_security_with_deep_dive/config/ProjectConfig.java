6b76f373f6f28e94425d0fe91a80106a9397e88b
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2025-01-18 22:09:07 +0300

    Custom realization authentication provider

--- src/main/java/kz/zhelezyaka/spring_security_with_deep_dive/config/ProjectConfig.java
+++ src/main/java/kz/zhelezyaka/spring_security_with_deep_dive/config/ProjectConfig.java
@@ -1,32 +1,27 @@
 package kz.zhelezyaka.spring_security_with_deep_dive.config;
 
+import kz.zhelezyaka.spring_security_with_deep_dive.providers.CustomAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class ProjectConfig {
+
+    private final CustomAuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults());
+        http.authenticationProvider(authenticationProvider);
         http.authorizeHttpRequests(
-                c -> c.anyRequest().authenticated()
-        );
-        var user = User.withUsername("Vladimir")
-                .password("123")
-                .authorities("read")
-                .build();
-        var userDetailsService =
-                new InMemoryUserDetailsManager(user);
-
-        http.userDetailsService(userDetailsService);
+                c -> c.anyRequest().authenticated());
         return http.build();
     }
 
---
+++ src/main/java/kz/zhelezyaka/spring_security_with_deep_dive/providers/CustomAuthenticationProvider.java
@@ -0,0 +1,33 @@
+package kz.zhelezyaka.spring_security_with_deep_dive.providers;
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+
+        if ("Vladimir".equals(username) && "123".equals(password)) {
+            return new UsernamePasswordAuthenticationToken(
+                    username, password, List.of());
+        } else {
+            throw new AuthenticationCredentialsNotFoundException("Error!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken
+                .class
+                .isAssignableFrom(authentication);
+    }
+}
