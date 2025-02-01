0f075508bb7b1142cac4f7fd7d15fc1e9e86653d
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2025-02-01 19:32:44 +0300

    Association of several responsibilities associated with the user

---
+++ src/main/java/kz/zhelezyaka/user_management/SpringSecurityWithDeepDiveApplication.java
@@ -0,0 +1,12 @@
+package kz.zhelezyaka.user_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithDeepDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithDeepDiveApplication.class, args);
+	}
+}
